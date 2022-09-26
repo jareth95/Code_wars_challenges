fc2ba9f71b72b535e8b9278ef686392bcc7a0478
Jareth <Jareth@mediatrader.com>
2022-09-26 10:53:08 +0100

    new

--- test.py
+++ test.py
@@ -3,6 +3,27 @@ import unittest
 import math
 
 
+'''
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+'''
+
+def square_digits(num):
+    return int(''.join(str(int(digit)**2) for digit in str(num)))
+
+
+class TestSquareDigits(unittest.TestCase):
+    @parameterized.expand([
+        [9119, 811181],
+        [0, 0],
+    ])
+    
+    def test_square_digits(self, num, answer):
+        self.assertEqual(square_digits(num), answer)
+
 
 '''
 Create a function which answers the question "Are you playing banjo?".
