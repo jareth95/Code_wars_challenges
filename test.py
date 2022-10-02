459070d90557c3234c5b231ea2c002c7dec6c7d0
Jareth <Jareth@mediatrader.com>
2022-10-02 10:15:52 +0100

    new

--- test.py
+++ test.py
@@ -4,6 +4,38 @@ import math
 
 
 '''
+This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+
+About factors
+Factors are numbers you can multiply together to get another number.
+
+2 and 3 are factors of 6 because: 2 * 3 = 6
+
+You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+You can use the mod operator (%) in most languages to check for a remainder
+For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+Note: base is a non-negative number, factor is a positive number.
+'''
+
+def check_for_factor(base, factor):
+    return base % factor == 0
+
+
+class TestCgeckForFactor(unittest.TestCase):
+    @parameterized.expand([
+        [10, 2, True],
+        [2450, 5, True],
+        [9, 2, False],
+        [2453, 5, False]
+    ])
+    
+    def test_check_for_factor(self, base, factor, answer):
+        self.assertEqual(check_for_factor(base, factor), answer)
+
+'''
 Build a function that returns an array of integers from n to 1 where n>0.
 
 Example : n=5 --> [5,4,3,2,1]
